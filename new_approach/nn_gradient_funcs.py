02a6aead128510a6087cf0af510f42eb7af2c62d
cjphs <schiffmann.c.h@gmail.com>
2023-04-18 23:20:33 +0200

    new approach

---
+++ new_approach/nn_gradient_funcs.py
@@ -0,0 +1,79 @@
+from point import Point
+from scipy.spatial import Voronoi
+from math import sqrt
+
+def generate_label_points(vor:Voronoi, omega:float) -> list:
+    
+    label_points = []
+    estimator_points = []
+
+    region_point_count = []
+
+    region_index = -1
+    for r in vor.regions:
+        region_index += 1
+        region_point_count.append(0)
+
+        l = r.copy()
+
+        # exclude -1 index vertices & use length 3 to form bisectors
+        if len(l) < 3 or -1 in l:
+            continue
+
+        center_x = 0
+        center_y = 0
+
+        point_index = 0
+        for point in l:
+            # find points
+            p0 = l[(point_index-1) % len(l)]
+            p1 = l[point_index]
+            p2 = l[(point_index+1) % len(l)]
+
+            print(f'{l}: handling points {p0}, {p1}, {p2}')
+
+            p0 = vor.vertices[p0].copy()
+            p1 = vor.vertices[p1].copy()
+            p2 = vor.vertices[p2].copy()
+
+            # centroid
+            center_x += p1[0]
+            center_y += p1[1]
+
+            # p1 is the middle point of the angle, set p1 as origin
+            p0[0] -= p1[0]
+            p0[1] -= p1[1]
+
+            p2[0] -= p1[0]
+            p2[1] -= p1[1]
+
+            # set new point to the middle of p0, p2
+            len_p0 = sqrt(p0[0]**2 + p0[1]**2)
+            len_p2 = sqrt(p2[0]**2 + p2[1]**2)
+
+            p0 = [p0[0]/len_p0, p0[1]/len_p0]
+            p2 = [p2[0]/len_p2, p2[1]/len_p2]
+
+            p3 = [(p0[0] + p2[0])/2, (p0[1] + p2[1])/2]
+            len_p3 = sqrt(p3[0]**2 + p3[1]**2)
+
+            p3[0] = p1[0] + omega * p3[0]/len_p3
+            p3[1] = p1[1] + omega * p3[1]/len_p3
+
+            # Add correct region as label
+            point = Point(p3[0], p3[1], region_index)
+
+            region_point_count[region_index] += 1
+
+            label_points.append(point)
+
+            point_index += 1
+
+        center_x /= len(l)
+        center_y /= len(l)
+
+        estimator_points.append(Point(center_x, center_y, region_index))
+    
+    return [label_points, estimator_points, region_point_count]
+
+\
