fc6958cc87cbe2752d74940e9c7d8547b1146a1c
cjphs <schiffmann.c.h@gmail.com>
2023-04-22 00:03:31 +0200

    iterations

--- thesis/__main__.py
+++ thesis/__main__.py
@@ -13,10 +13,10 @@ def voronoi_from_points(points:list[Point]) -> Voronoi:
 
 if __name__ == "__main__":
 
-    omega = 1
-    phi = .025
+    omega = .001
+    phi = .00025
 
-    vor = generate_random_voronoi(20,xmin=0,xmax=100,ymin=0,ymax=100)
+    vor = generate_random_voronoi(20,xmin=0,xmax=1,ymin=0,ymax=1)
     voronoi_plot_2d(vor)
 
     points = generate_label_points(vor, omega)
@@ -27,24 +27,25 @@ if __name__ == "__main__":
     original_approximation = voronoi_from_points(estimator_points)
     voronoi_plot_2d(original_approximation, line_colors='orange', line_alpha=0.4, ax=plt.gca(), show_vertices=False, show_points=False)
 
+    iterations = 0
+
     done = False
     while(not done):
         nudged = nudge_estimators(estimator_points, label_points, phi, pull=True, push=True)
+        iterations += 1
 
         if not nudged:
-            print('all labels satisfied')
+            print(f'all labels satisfied after {iterations} iterations')
             done = True
 
         for p in estimator_points:
             p.update_plot()
 
-        #plt.waitforbuttonpress()
         plt.pause(0.001)
 
-    plt.waitforbuttonpress()
-
+    print('uh?')
     # generate new voronoi diagram from final estimator point positions
-    new_vor = Voronoi([[p.x, p.y] for p in estimator_points])
-    voronoi_plot_2d(new_vor, ax=plt.gca(), line_alpha=.25)
+    new_vor = voronoi_from_points(estimator_points)
+    voronoi_plot_2d(new_vor, ax=plt.gca(), line_alpha=.5, line_colors='blue')
 
     plt.show()
