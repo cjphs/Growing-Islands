b34210ce4e5b59f376ec485678d2a60528de7d7d
cjphs <schiffmann.c.h@gmail.com>
2023-04-21 09:22:08 +0200

    voronoi range

--- thesis/__main__.py
+++ thesis/__main__.py
@@ -4,20 +4,28 @@ from scipy.spatial import Voronoi, voronoi_plot_2d
 from input_generation.random_voronoi import generate_random_voronoi
 from nn_gradient_funcs import generate_label_points
 
+from geometry.point import Point
+
 from nn_gradient import nudge_estimators
+
+def voronoi_from_points(points:list[Point]) -> Voronoi:
+    return Voronoi([[p.x, p.y] for p in points])
 
 if __name__ == "__main__":
 
-    omega = 0.01
-    phi = 0.00015
+    omega = 1
+    phi = .025
 
-    vor = generate_random_voronoi(60)
+    vor = generate_random_voronoi(20,xmin=0,xmax=100,ymin=0,ymax=100)
     voronoi_plot_2d(vor)
 
     points = generate_label_points(vor, omega)
-    
+
     label_points = points[0]
     estimator_points = points[1]
+
+    original_approximation = voronoi_from_points(estimator_points)
+    voronoi_plot_2d(original_approximation, line_colors='orange', line_alpha=0.4, ax=plt.gca(), show_vertices=False, show_points=False)
 
     done = False
     while(not done):
--- thesis/input_generation/random_voronoi.py
+++ thesis/input_generation/random_voronoi.py
@@ -1,15 +1,15 @@
 from scipy.spatial import Voronoi
 from random import uniform as U
 
-def generate_random_points(num_points:int=20) -> list[list[int, int]]:
+def generate_random_points(num_points:int=20, xmin=0, xmax=1, ymin=0, ymax=1) -> list[list[int, int]]:
     points = []
     while(num_points > 0):
-        points.append([U(0,1), U(0,1)])
+        points.append([U(xmin,xmax), U(ymin,ymax)])
         num_points -= 1
     return points
 
-def generate_random_voronoi(num_points:int=20) -> Voronoi:
-    points = generate_random_points(num_points)
+def generate_random_voronoi(num_points:int=20, xmin=0, xmax=1, ymin=0, ymax=1) -> Voronoi:
+    points = generate_random_points(num_points, xmin, xmax, ymin, ymax)
     vor = Voronoi(points)
     return vor
 
