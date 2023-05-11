24f849378c323912537965d606ca9d4bd112c97b
Christopher Schiffmann <72268429+cjphs@users.noreply.github.com>
2023-05-11 23:36:44 +0200

    divorce voronoi approximation.py from thesis.py

--- thesis/thesis.py
+++ thesis/thesis.py
@@ -1,14 +1,11 @@
 from matplotlib import pyplot as plt
-from scipy.spatial import Voronoi, voronoi_plot_2d
+from scipy.spatial import voronoi_plot_2d
 
 from input_generation.voronoi_funcs import generate_random_voronoi, voronoi_from_points
 
+from voronoi_approximation import VoronoiApproximation
+
 from preprocessing import generate_label_points
-from nudging import nudge_estimators
-
-from geometry.point import Point
-
-from math import ceil, floor
 import sys
 
 import os
@@ -34,7 +31,8 @@ def get_arg(args, arg_name):
 def main():
     gui = True
 
-    omega, phi= .98, .0002
+    omega   = .95
+    phi     = .00025
 
     num_points = 50
 
@@ -67,12 +65,12 @@ def main():
         plt.title(label="Input diagram")
         plt.waitforbuttonpress(0)
 
-    label_points, estimator_points = generate_label_points(vor, omega)
+    approximation = VoronoiApproximation(vor, omega, phi)
 
-    original_approximation = voronoi_from_points(estimator_points)
+    original_approximation = voronoi_from_points(approximation.estimator_points)
 
     if gui:
-        for p in estimator_points:
+        for p in approximation.estimator_points:
             plt.plot(p.x, p.y, "b+", alpha=.2)
             p.x = min(max(p.x, xmin), xmax)
             p.y = min(max(p.y, ymin), ymax)
@@ -85,62 +83,14 @@ def main():
         plt.gcf().canvas.mpl_connect('key_press_event', on_press)
         plt.title(label="Nudging generator approximations...")
 
-    iterations = 0
-    points_satisfied = []
-    begin = datetime.now()
-
-    done = False
-    while(not done):
-        nudged = nudge_estimators(estimator_points, label_points, phi, pull=True, push=True)
-        iterations += 1
-
-        if not nudged:
-            plt.title(label="All label points satisfied!")
-            done = True
-
-        if gui:
-            for p in estimator_points:
-                p.update_plot()
-
-        satisfied_count = 0
-        for l in label_points:
-            if l.satisfied:
-                satisfied_count += 1
-
-        satisfied_percentage = satisfied_count/len(label_points)
-
-        points_satisfied.append(satisfied_percentage)
-
-        percent_bar_length = os.get_terminal_size().columns
-
-        m = floor(satisfied_percentage * percent_bar_length)
-
-        percent_bar = m * "█" + (percent_bar_length - m) * "░"
-
-        progress = f"{percent_bar}"
-
-        sys.stdout.write("\r" + progress)
-        sys.stdout.flush()
-
-        if gui:
-            plt.pause(1e-10)
-
-    end = datetime.now()
-
-    sys.stdout.write("\r" + f"Finished in {end - begin} ({iterations} iterations)")
-    sys.stdout.flush()
-    print()
+    approximation.do_thingy()
 
     # generate new voronoi diagram from final estimator point positions
-    new_vor = voronoi_from_points(estimator_points)
+    new_vor = voronoi_from_points(approximation.estimator_points)
     voronoi_plot_2d(new_vor, ax=plt.gca(), line_alpha=.5, line_colors='blue')
     enforce_plot_scale(xmin,xmax,ymin,ymax)
 
     plt.pause(1e-10)
-
-    plt.figure()
-    plt.plot(points_satisfied, color='black')
-    plt.title(label="Percentage of label points satisfied over time")
 
     plt.show()
 
---
+++ thesis/voronoi_approximation.py
@@ -0,0 +1,74 @@
+from matplotlib import pyplot as plt
+from nudging import nudge_estimators
+from preprocessing import generate_label_points
+
+import sys
+import os
+from datetime import datetime
+from math import floor
+
+
+class VoronoiApproximation:
+
+    def __init__(self, diagram, omega, phi):
+        self.diagram = diagram
+        self.omega = omega
+        self.phi = phi
+
+        self.gui = False
+
+        self.label_points, self.estimator_points = generate_label_points(diagram, omega)
+
+
+    def do_thingy(self):
+        iterations = 0
+        points_satisfied = []
+        begin = datetime.now()
+
+        done = False
+        while(not done):
+            nudged = nudge_estimators(self.estimator_points, self.label_points, self.phi, pull=True, push=True)
+            iterations += 1
+
+            if not nudged:
+                plt.title(label="All label points satisfied!")
+                done = True
+
+            if self.gui:
+                for p in self.estimator_points:
+                    p.update_plot()
+
+            satisfied_count = 0
+            for l in self.label_points:
+                if l.satisfied:
+                    satisfied_count += 1
+
+            satisfied_percentage = satisfied_count/len(self.label_points)
+
+            points_satisfied.append(satisfied_percentage)
+
+            percent_bar_length = os.get_terminal_size().columns
+
+            m = floor(satisfied_percentage * percent_bar_length)
+
+            percent_bar = m * "█" + (percent_bar_length - m) * "░"
+
+            progress = f"{percent_bar}"
+
+            sys.stdout.write("\r" + progress)
+            sys.stdout.flush()
+
+            if self.gui:
+                plt.pause(1e-10)
+
+        end = datetime.now()
+
+        sys.stdout.write("\r" + f"Finished in {end - begin} ({iterations} iterations)")
+        sys.stdout.flush()
+        print()
+
+        plt.figure()
+        plt.plot(points_satisfied, color='black')
+        plt.title(label="Percentage of label points satisfied over time")
+
+        return self.estimator_points
